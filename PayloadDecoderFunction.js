574756a97cda2bfac54d0f121818a9d688bc8905
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2019-05-02 14:18:21 +0200

    Made bytesToInt simpler and beautyfied the file

--- PayloadDecoderFunction.js
+++ PayloadDecoderFunction.js
@@ -1,51 +1,46 @@
-function Decoder(bytes, port) {  
-  var result = {
-    func: bytes[0],
-    ct: bytes[1],
-    channel: bytesToInt([bytes[2], bytes[3]], false),
-    channelCount: bytes.length/4 - 1,
-    payload : bytes, 
-  };
+function Decoder(bytes, port) {
+    var result = {
+        func: bytes[0],
+        ct: bytes[1],
+        channel: bytesToInt(bytes.slice(2, 4)),
+        channelCount: bytes.length / 4 - 1,
+        payload: bytes,
+    };
 
-  for (var i = 1; i <= result.channelCount; i++) {
-    var MSBIndex = i * 4;
-    var byteValue = bytes.slice(MSBIndex, MSBIndex + 4);
+    for (var i = 1; i <= result.channelCount; i++) {
+        var msbIndex = i * 4;
+        var byteValue = bytes.slice(msbIndex, msbIndex + 4);
 
-    result['Channel_' + i] = bytesToFloat(byteValue);
-  }
+        result['Channel_' + i] = bytesToFloat(byteValue);
+    }
 
-  return result;
+    return result;
 }
 
 // Based on https://stackoverflow.com/a/37471538 by Ilya Bursov
 function bytesToFloat(bytes) {
-  // JavaScript bitwise operators yield a 32 bits integer, not a float.
-  // Assume MSB (most significant byte first).
-  var bits = bytes[0]<<24 | bytes[1]<<16 | bytes[2]<<8 | bytes[3];
-  var sign = (bits>>>31 === 0) ? 1.0 : -1.0;
-  var e = bits>>>23 & 0xff;
-  var m = (e === 0) ? (bits & 0x7fffff)<<1 : (bits & 0x7fffff) | 0x800000;
-  var f = sign * m * Math.pow(2, e - 150);
-  return f;
-}  
-  
-function bytesToInt(bytes, isLittleEndian) {
-  //var totalBits = (signBits + exponentBits + fractionBits);
-  var binary = "";
+    // JavaScript bitwise operators yield a 32 bits integer, not a float.
+    // Assume MSB (most significant byte first).
+    var bits = bytes[0] << 24 | bytes[1] << 16 | bytes[2] << 8 | bytes[3];
+    var sign = (bits >>> 31 === 0) ? 1.0 : -1.0;
+    var e = bits >>> 23 & 0xff;
+    var m = (e === 0) ? (bits & 0x7fffff) << 1 : (bits & 0x7fffff) | 0x800000;
+    var f = sign * m * Math.pow(2, e - 150);
+    return f;
+}
 
-  for (var i = 0, l = bytes.length; i < l; i++) {
-    var bits = bytes[i].toString(2);
+function bytesToInt(bytes) {
+    var binary = "";
 
-    while (bits.length < 8) {
-      bits = "0" + bits;
+    for (var i = 0, l = bytes.length; i < l; i++) {
+        var bits = bytes[i].toString(2);
+
+        while (bits.length < 8) {
+            bits = "0" + bits;
+        }
+
+        binary = binary + bits;
     }
 
-    if (isLittleEndian) {
-      binary = bits + binary;
-    } else {
-      binary = binary + bits;
-    }
-  }
-
-  return binary;
+    return binary;
 }
