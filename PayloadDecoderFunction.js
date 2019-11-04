efcb3b225b7c7527f91f7d0f9c61fb9f809fd282
Benjamin Ricchiuto <ricchben@students.zhaw.ch>
2019-11-04 11:23:02 +0100

    Updated to new specifications

--- PayloadDecoderFunction.js
+++ PayloadDecoderFunction.js
@@ -1,21 +1,41 @@
 function Decoder(bytes, port) {
+    var functionCode = bytes[0];
     var result = {
-        func: bytes[0],
-        ct: bytes[1],
-        channel: bytesToInt(bytes.slice(2, 4)),
-        channelCount: bytes.length / 4 - 1,
-        payload: bytes,
-        portNumber: port
+        func: functionCode,
+        port: port,
+        payload: bytes.map(function (byte) { return pad(byte.toString(16).toUpperCase(), 2); }).join('')
     };
+
+    if (functionCode === 12) {
+        fillupDecodedDeviceInformation(bytes, result);
+    } else {
+        fillupDecodedMeasurements(bytes, result);
+    }
+
+    return result;
+}
+
+function fillupDecodedDeviceInformation(bytes, result) {
+    result.battery_voltage = bytesToFloat(bytes.slice(14, 18));
+    result.battery_capacity_percentage = bytes[18];
+    result.humidity_percentage = bytes[19];
+    result.class_group_text = pad(bytes[2], 2) + '.' + pad(bytes[3], 2);
+    result.sw_version_text = pad(bytes[4], 2) + '.' + pad(bytes[5], 2);
+    result.serial_number = bytesToInt(bytes.slice(6, 10));
+    result.device_local_datetime = bytesToDate(bytes.slice(10, 14));
+}
+
+function fillupDecodedMeasurements(bytes, result) {
+    result.ct = bytes[1];
+    result.channel = bytesToBinaryString(bytes.slice(2, 4), 2);
+    result.channelCount = result.channel.match(/1/g).length;
 
     for (var i = 1; i <= result.channelCount; i++) {
         var msbIndex = i * 4;
         var byteValue = bytes.slice(msbIndex, msbIndex + 4);
 
-        result['Channel_' + i] = bytesToFloat(byteValue);
+        result[deviceTypesToChannelNames[result.ct][i - 1]] = bytesToFloat(byteValue);
     }
-
-    return result;
 }
 
 // Based on https://stackoverflow.com/a/37471538 by Ilya Bursov
@@ -30,14 +50,14 @@ function bytesToFloat(bytes) {
     return f;
 }
 
-function bytesToInt(bytes) {
-    var binary = "";
+function bytesToBinaryString(bytes) {
+    var binary = '';
 
     for (var i = 0, l = bytes.length; i < l; i++) {
         var bits = bytes[i].toString(2);
 
         while (bits.length < 8) {
-            bits = "0" + bits;
+            bits = '0' + bits;
         }
 
         binary = binary + bits;
@@ -45,3 +65,102 @@ function bytesToInt(bytes) {
 
     return binary;
 }
+
+function bytesToInt(bytes) {
+    var binaryString = bytesToBinaryString(bytes);
+    return parseInt(binaryString, 2);
+}
+
+function bytesToDate(bytes) {
+    var zero = new Date('2000-01-01T00:00:00.000Z');
+    var time = bytesToInt(bytes);
+    var date = new Date(zero.getTime() + time * 1000);
+
+    var year = date.getUTCFullYear();
+    var month = date.getUTCMonth() + 1;
+    var day = date.getUTCDate();
+    var hours = date.getUTCHours();
+    var minutes = date.getUTCMinutes();
+    var seconds = date.getUTCSeconds();
+
+    return year + '-' + pad(month, 2) + '-' + pad(day, 2) + ' ' + pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(seconds, 2);
+}
+
+function pad(num, size) {
+    var s = num.toString();
+    while (s.length < size) s = '0' + s;
+    return s;
+}
+
+var map = {
+    1: 'Pd (P1-P2)',
+    2: 'P1',
+    3: 'P2',
+    4: 'T',
+    5: 'TOB1',
+    6: 'TOB2',
+    7: 'PBaro',
+    8: 'TBaro',
+    9: 'Volt Inp. 1',
+    10: 'Volt Inp. 2',
+    11: 'Pd (P1-PBaro)',
+    12: 'Conductivity Tc',
+    13: 'Conductivity raw',
+    14: 'T (Conductivity)',
+    15: 'P1 (2)',
+    16: 'P1 (3)',
+    17: 'P1 (4)',
+    18: 'P1 (5)',
+    19: 'Counter input',
+    20: 'SDI12 CH1',
+    21: 'SDI12 CH2',
+    22: 'SDI12 CH3',
+    23: 'SDI12 CH4',
+    24: 'SDI12 CH5',
+    25: 'SDI12 CH6',
+    26: 'SDI12 CH7',
+    27: 'SDI12 CH8',
+    28: 'SDI12 CH9',
+    29: 'SDI12 CH10',
+    30: 'TOB1 (2)',
+    31: 'TOB1 (3)',
+    32: 'TOB1 (4)',
+    33: 'TOB1 (5)',
+    34: 'E',
+    35: 'F',
+    36: 'G',
+    37: 'mH20 (Pbaro)',
+    38: 'mH20 (P1-P2)',
+    39: 'mH20 (P1-P3)',
+    40: 'mH20 (P1-P4)',
+    41: 'mH20 (P1-P5)',
+    42: 'Conductivity Tc (2)',
+    43: 'Conductivity Tc (3)',
+    44: 'T (Conductivity) (2)',
+    45: 'T (Conductivity) (3)',
+    46: 'P2 (2)',
+    47: 'TOB2 (2)',
+    48: 'AquaMaster Flow Rate',
+    49: 'AquaMaster Pressure',
+    50: 'AquaMaster Custom Flow Units',
+    51: 'AquaMaster External Supply Voltage',
+    52: 'Tank Content 1',
+    53: 'Tank Content 2',
+    54: 'Tank Content 3',
+};
+
+var deviceTypesToChannelNames = {
+    0: [map[1], map[2], map[3], map[4], map[5], map[6]],
+    1: [map[1], map[2], map[3], map[4], map[5], map[6]],
+    2: [map[11], map[2], map[3], map[4], map[5], map[6], map[7], map[8]],
+    3: [map[11], map[2], map[3], map[4], map[5], map[6], map[7], map[8]],
+    4: [map[1], map[2], map[3], map[4], map[5], map[6], map[7], map[8], map[9], map[10]],
+    5: [map[11], map[2], map[3], map[4], map[5], map[6], map[7], map[8], map[9], map[10], map[11]],
+    6: [map[1], map[2], map[3], map[4], map[5], map[6], map[7], map[8], map[9], map[10], map[15], map[16], map[17], map[18], map[19]],
+    7: [map[7], map[8], map[9], map[10], map[20], map[21], map[22], map[23], map[24], map[25], map[26], map[27], map[28], map[29]],
+    8: [map[2], map[5], map[15], map[30], map[16], map[31], map[17], map[32], map[18], map[33], map[9], map[10], map[7], map[8], map[19]],
+    9: [map[1], map[2], map[3], map[14], map[5], map[6], map[7], map[8], map[9], map[10], map[12], map[13]],
+    10: [map[11], map[2], map[3], map[14], map[5], map[6], map[7], map[8], map[9], map[10], map[12], map[13]],
+    11: [map[2], map[5], map[12], map[14], map[15], map[30], map[42], map[44], map[16], map[31], map[43], map[45], map[7], map[8], map[19]],
+    13: [map[2], map[3], map[5], map[6], map[15], map[46], map[30], map[47], map[7], map[8], map[9], map[10], map[19]],
+};
--- README.md
+++ README.md
@@ -1,29 +1,32 @@
 # KellerAgTheThingsNetworkPayloadDecoder
+
 Payload decoder function to decode a LoRa payload from a KELLER device to meaningful values viewable on console.thethingsnetwork.org  
 
-For a more detailed explanation please please visit to https://docs.kolibricloud.ch/sending-technology/lora-technology/keller-lora-payload/
+For a more detailed explanation please please visit to <https://docs.kolibricloud.ch/sending-technology/lora-technology/keller-lora-payload/>
 
 To use it:
- * Step 1: Open console.thethingsnetwork.org and go to your "application" 
 
- * Step 2: Choose "Payload Formats"
+* Step 1: Open console.thethingsnetwork.org and go to your "application"
+* Step 2: Choose "Payload Formats"
 ![alt text](https://raw.githubusercontent.com/KELLERAGfuerDruckmesstechnik/KellerAgTheThingsNetworkPayloadDecoder/master/TheThingsNetworkApplicationPayloadFormat.png "https://console.thethingsnetwork.org/applications/{your_application_name}/payload-formats")
 
- * Step 3: Copy-Paste the source code from PayloadDecoderFunction.js into the [Payload  formats] -> [decoder] text box and press the [save payload function] button:
+* Step 3: Copy-Paste the source code from PayloadDecoderFunction.js into the [Payload  formats] -> [decoder] text box and press the [save payload function] button:
 ![alt text](https://raw.githubusercontent.com/KELLERAGfuerDruckmesstechnik/KellerAgTheThingsNetworkPayloadDecoder/master/ExamplePayloadFunctionInTTN.png "TheThingsNetworkApplicationPayloadFormat.png")
- * Step 4: Verify by entering an example Payload string such as: ```010500D3BF7845343C5FE44041AE22803F7BC4B141A86666```
- this should result into:
- ```json
- {
-  "Channel_1": -0.9698059558868408,
-  "Channel_2": 0.013665258884429932,
-  "Channel_3": 21.766845703125,
-  "Channel_4": 0.9834700226783752,
-  "Channel_5": 21.049999237060547,
-  "channel": "0000000011010011",
-  "channelCount": 5,
-  "ct": 5,
-  "func": 1,
-  "payload": "AQUA0794RTQ8X+RAQa4igD97xLFBqGZm"
+* Step 4: Verify by entering an example Payload string such as: ```010500D3BF7845343C5FE44041AE22803F7BC4B141A86666```
+this should result into:
+
+```json
+{
+    "P1": 0.013665258884429932,
+    "P2": 21.766845703125,
+    "Pd (P1-PBaro)": -0.9698059558868408,
+    "T": 0.9834700226783752,
+    "TOB1": 21.049999237060547,
+    "channel": "0000000011010011",
+    "channelCount": 5,
+    "ct": 5,
+    "func": 1,
+    "payload": "010500D3BF7845343C5FE44041AE22803F7BC4B141A86666",
+    "port": 1
 }
 ```
