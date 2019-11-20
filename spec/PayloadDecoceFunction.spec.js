3858ea0529c5d9bf05caf24ffeaa0eb1ff9661e3
Benjamin Ricchiuto <ricchben@students.zhaw.ch>
2019-11-20 07:41:33 +0100

    Added spec

--- spec/PayloadDecoceFunction.spec.js
+++ spec/PayloadDecoceFunction.spec.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 eval(fs.readFileSync('PayloadDecoderFunction.js')+'');
 
 describe('PayloadDecoderFunction', function() {  
-    it('should decode a given payload correctly (1/4)', function() {
+    it('should decode a given payload correctly (1/5)', function() {
         var payload = [0x0C, 0x01, 0x13, 0x00, 0x13, 0x26, 0x00, 0x00, 0x00, 0x01, 0x25, 0x4D, 0x4F, 0xCA, 0x40, 0xA4, 0xFC, 0x2B, 0x0D, 0x10];
         var result = Decoder(payload, 4);
   
@@ -20,7 +20,7 @@ describe('PayloadDecoderFunction', function() {
         });
     });
 
-    it('should decode a given payload correctly (2/4)', function() {  
+    it('should decode a given payload correctly (2/5)', function() {  
         var payload = [0x01, 0x02, 0x00, 0xD3, 0xFF, 0xFF, 0xFF, 0xFF, 0x3C, 0x77, 0x2C, 0xAC, 0x41, 0xAF, 0x51, 0x80, 0x3F, 0x71, 0x70, 0xF8, 0x41, 0xAA, 0xCC, 0xCD];
         var result = Decoder(payload, 1);
   
@@ -39,7 +39,7 @@ describe('PayloadDecoderFunction', function() {
         });
     });
 
-    it('should decode a given payload correctly (3/4)', function() {  
+    it('should decode a given payload correctly (3/5)', function() {  
         var payload = [0x01, 0x09, 0x0C, 0xD7, 0xFF, 0xFF, 0xFF, 0xFF, 0x3A, 0x99, 0xF8, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0x41, 0xA1, 0xC4, 0x30, 0x3F, 0x77, 0x8F, 0xEF, 0x41, 0xAA, 0x14, 0x7B, 0x3B, 0x43, 0x66, 0xA9, 0x3B, 0x2F, 0x7A, 0x1A];
         var result = Decoder(payload, 1);
   
@@ -61,7 +61,7 @@ describe('PayloadDecoderFunction', function() {
         });
     });
 
-    it('should decode a given payload correctly (4/4)', function() {  
+    it('should decode a given payload correctly (4/5)', function() {  
         var payload = [0x01, 0x02, 0x00, 0xD2, 0x3A, 0x28, 0x00, 0x00, 0x41, 0xB4, 0x66, 0x66, 0x3F, 0x76, 0x7C, 0x07, 0x41, 0xB6, 0x8F, 0x5C];
         var result = Decoder(payload, 1);
   
@@ -76,7 +76,26 @@ describe('PayloadDecoderFunction', function() {
             'func': 1,
             'payload': '010200D23A28000041B466663F767C0741B68F5C',
             'port': 1
-          });
+        });
+    });
+
+    it('should decode a given payload correctly (5/5)', function() {
+        var payload = [0x01, 0x02, 0x00, 0xD3, 0xFF, 0xFF, 0xFF, 0xFF, 0xB9, 0x90, 0x0E, 0x00, 0x41, 0xAE, 0x81, 0x80, 0x3F, 0x77, 0x66, 0xA5, 0x41, 0xA8, 0xF5, 0xC3];
+        var result = Decoder(payload, 1);
+
+        expect(result).toEqual({
+            'Pd (P1-PBaro)': -6.805646932770577e+38,
+            'P1': -0.00027476251125335693,
+            'TOB1': 21.813232421875,
+            'PBaro': 0.9664099812507629,
+            'TBaro': 21.1200008392334,
+            'func': 1,
+            'port': 1,
+            'payload': '010200D3FFFFFFFFB9900E0041AE81803F7766A541A8F5C3',
+            'ct': 2,
+            'channel': '0000000011010011',
+            'channelCount': 5
+        });
     });
 
     it('should convert bytes to float corrrectly', function() {
